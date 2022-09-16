ced405d90b4b117c5923fe1f01aa0f121e041c3f
LevitatingBusinessMan <git@reinfernhout.xyz>
2022-09-16 19:21:06 +0200

    removed test folder because of stupid submission instructions

--- handin/README.txt
+++ handin/README.txt
@@ -7,6 +7,6 @@ This includes the main assignemnt and both bonus assignments.
 Additions were made to `dup.py` and `sort.py`, so those files are included in this zip.
 
 There is a test file called `test.py`.
-This uses the test files in the `test` folder.
+This uses two test text files `tbrian.txt` and `tgrail.txt`.
 The test file does not output anything by default.
 But one can read it to see the assertions.
--- handin/test.py
+++ handin/test.py
@@ -1,35 +1,35 @@
 import util, pearl
 
-pairs = util.word_pairs(["test/grail.txt", "test/brian.txt"])
+pairs = util.word_pairs(["tgrail.txt", "tbrian.txt"])
 
 assert pearl.make_table(pairs) == [
-    ['dead', ['test/brian.txt', 'test/grail.txt']],
-    ['eunt', ['test/brian.txt']],
-    ['spank', ['test/grail.txt']],
+    ['dead', ['tbrian.txt', 'tgrail.txt']],
+    ['eunt', ['tbrian.txt']],
+    ['spank', ['tgrail.txt']],
 ]
 
 assert pearl.make_counted_table(pairs) == [
     ['dead', [
-        ['test/grail.txt', 3],
-        ['test/brian.txt', 1],
+        ['tgrail.txt', 3],
+        ['tbrian.txt', 1],
     ]],
     ['eunt', [
-        ['test/brian.txt', 2],
+        ['tbrian.txt', 2],
     ]],
     ['spank', [
-        ['test/grail.txt', 7],
+        ['tgrail.txt', 7],
     ]],
 ]
 
 assert pearl.make_density_table(pairs) == [
     ['dead', [
-        ['test/brian.txt', 0.3333333333333333],
-        ['test/grail.txt', 0.3]
+        ['tbrian.txt', 0.3333333333333333],
+        ['tgrail.txt', 0.3]
     ]],
     ['eunt', [
-        ['test/brian.txt', 0.6666666666666666],
+        ['tbrian.txt', 0.6666666666666666],
     ]],
     ['spank', [
-        ['test/grail.txt', 0.7],
+        ['tgrail.txt', 0.7],
     ]]
 ]
